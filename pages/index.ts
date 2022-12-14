a1a16d3e990788b2f136375912fdb41339ec3805
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2022-12-15 00:03:21 +0100

    a

--- pages/index.ts
+++ pages/index.ts
@@ -3,7 +3,7 @@ import { initSettings } from "../src"
 
 let settings = initSettings((await import("../package.json")).name);
 
-let TestSetting = settings.makeSetting('Test', 'asdf')
+let TestSetting = settings.makeStringSetting('Test', 'asdf')
 if (TestSetting) {
     let value = document.createElement('input');
     document.body.appendChild(value);
