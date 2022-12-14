f5483c1192553d4001ecf1ec35bb2e7feb08015a
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2022-12-15 00:03:03 +0100

    Initial release Fixes #1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ValueLimited, Limiter } from "@chocolatelib/value";
+import { ValueLimited, Limiter, Value, ValueLimitedNumber, ValueLimitedString, EnumList } from "@chocolatelib/value";
 
 let bottomGroups: { [key: string]: SettingsGroup } = {};
 
@@ -24,23 +24,40 @@ export class SettingsGroup {
             console.warn('Sub group already registered ' + name);
             return undefined
         } else {
-
             return this.subGroups[name] = new SettingsGroup(this.path + '/' + name);
         }
     }
 
-    makeSetting<T extends boolean | number | string>(name: string, defaultValue: T, limiter: Limiter<T> = (val) => val) {
+    /**Makes a boolean setting */
+    makeBooleanSetting(name: string, defaultValue: boolean) {
         if (name in this.settings) {
-            console.warn('Settings already registered ' + name);
-            return undefined;
-        } else {
-            let saved = localStorage[this.path + '/' + name];
-            let setting = new ValueLimited<T>((saved ? JSON.parse(saved) : defaultValue), limiter);
-            setting.addListener((val) => {
-                localStorage[this.path + '/' + name] = JSON.stringify(val);
-            }, !saved)
-            //@ts-ignore
-            return setting;
+            throw new Error('Settings already registered ' + name);
         }
+        let saved = localStorage[this.path + '/' + name];
+        let setting = new Value<boolean>(saved ? JSON.parse(saved) : defaultValue);
+        setting.addListener((val) => { localStorage[this.path + '/' + name] = JSON.stringify(val); }, !saved)
+        return setting;
+    }
+
+    /**Makes a number setting */
+    makeNumberSetting(name: string, defaultValue: number, min?: number, max?: number, step?: number, limiters?: Limiter<number>[]) {
+        if (name in this.settings) {
+            throw new Error('Settings already registered ' + name);
+        }
+        let saved = localStorage[this.path + '/' + name];
+        let setting = new ValueLimitedNumber(saved ? JSON.parse(saved) : defaultValue, min, max, step, limiters);
+        setting.addListener((val) => { localStorage[this.path + '/' + name] = JSON.stringify(val); }, !saved)
+        return setting;
+    }
+
+    /**Makes a string setting */
+    makeStringSetting(name: string, defaultValue: string, enums?: EnumList, maxLength?: number, maxByteLength?: number, limiters?: Limiter<string>[]) {
+        if (name in this.settings) {
+            throw new Error('Settings already registered ' + name);
+        }
+        let saved = localStorage[this.path + '/' + name];
+        let setting = new ValueLimitedString(saved ? JSON.parse(saved) : defaultValue, enums, maxLength, maxByteLength, limiters);
+        setting.addListener((val) => { localStorage[this.path + '/' + name] = JSON.stringify(val); }, !saved)
+        return setting;
     }
 }
