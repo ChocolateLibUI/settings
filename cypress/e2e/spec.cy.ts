0f5bfd05b0250fa03cbf7ec3a401fca7ea1ed18a
Chocolateandmilkwin <52867415+Chocolateandmilkwin@users.noreply.github.com>
2023-03-14 13:07:39 +0100

    Chocolateandmilkwin/issue3 (#4)

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,14 +1,16 @@
 /// <reference types="cypress" />
 import { initSettings } from "../../src"
 import { name } from "../../package.json"
+import { createState } from "@chocolatelib/state";
 
 describe('empty spec', () => {
   it('Initialise settings', () => {
     expect(localStorage[name + '/test1']).to.equal(undefined);
     let sets = initSettings(name, 'Test Settings', 'Settings for testing');
-    let test1 = sets.makeStringSetting('test1', 'test', 'test', 'value')
+    let { state, set } = createState('value')
+    let test1 = sets.addState('test1', state, set);
     expect(JSON.parse(localStorage[name + '/test1'])).to.equal('value');
-    test1.set = 'value2';
+    set('value2');
     expect(JSON.parse(localStorage[name + '/test1'])).to.equal('value2');
     localStorage[name + '/test1'] = JSON.stringify('value3')
     cy.reload().then(() => {
@@ -18,12 +20,13 @@ describe('empty spec', () => {
   it('Initialise settings with async value', async () => {
     expect(localStorage[name + '/test1']).to.equal(undefined);
     let sets = initSettings(name, 'Test Settings', 'Settings for testing');
-    let test1 = sets.makeStringSetting('test1', 'test', 'test', new Promise((a) => {
+    let { state, set } = createState('value')
+    let test1 = sets.addState('test1', state, set, new Promise((a) => {
       setTimeout(a, 400, 'value');
     }))
     await new Promise((a) => { setTimeout(a, 600) });
     expect(JSON.parse(localStorage[name + '/test1'])).to.equal('value');
-    test1.set = 'value2';
+    set('value2');
     expect(JSON.parse(localStorage[name + '/test1'])).to.equal('value2');
     localStorage[name + '/test1'] = JSON.stringify('value3')
     cy.reload().then(() => {
